6947ff67b19efe777c3b43233226789d73b64080
Liutauras Medziunas <liutauras.medziunas@visma.com>
2017-06-05 17:29:35 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InExchange.Format.UBL;
+
+namespace FluentUbl
+{
+
+  public interface IOrderBuilder
+  {
+    IOrderBuilder BuildId(string id);
+    IOrderBuilder BuildIssueDate(DateTime date);
+    IOrderBuilder BuildIssueTime(DateTime time);
+    IOrderBuilder BuildBuyerParty(Func<IBuyerPartyBuilder, IBuyerPartyBuilder> buyerPartyBuilder);
+    IOrderBuilder BuildLines(Func<IEnumerable<ILineBuilder>> lineBuilder);
+    UblOrder GetUblOrder();
+  }
+
+  public interface IBuyerPartyBuilder
+  {
+    IBuyerPartyBuilder SetName(string name);
+    IBuyerPartyBuilder SetGln(string gln);
+  }
+
+  public interface ILinesBuilder
+  {
+    ILineBuilder BuildLine();
+  }
+
+  public interface ILineBuilder
+  {
+    ILineBuilder SetId(string id);
+    ILineBuilder SetDescription(string description);
+  }
+
+  public class OrderBuilder : IOrderBuilder
+  {
+    private UblOrder _ublOrder = UblDocument.Create<UblOrder>(UblProfiles.Bii6ProcurementExtended);
+
+    public OrderBuilder()
+    {
+      _ublOrder.BuyerCustomerParty = new UblCustomerParty();
+    }
+
+    public IOrderBuilder BuildId(string id)
+    {
+      _ublOrder.Id = new UblIdentifier(id);
+      return this;
+    }
+
+    public IOrderBuilder BuildIssueDate(DateTime date)
+    {
+      _ublOrder.IssueDate = new UblDate(date);
+      return this;
+    }
+
+    public IOrderBuilder BuildIssueTime(DateTime time)
+    {
+      _ublOrder.IssueTime = new UblTime(time);
+      return this;
+    }
+
+    public IOrderBuilder BuildBuyerParty(Func<IBuyerPartyBuilder, IBuyerPartyBuilder> buyerPartyBuilder)
+    {
+      buyerPartyBuilder.Invoke(new BuyerPartyBuilder(_ublOrder.BuyerCustomerParty));
+      return this;
+    }
+
+    public IOrderBuilder BuildLines(Func<IEnumerable<ILineBuilder>> lineBuilder)
+    {
+      _ublOrder.OrderLine = new List<UblOrderLine>();
+      foreach (LineBuilder builder in lineBuilder())
+      {
+        _ublOrder.OrderLine.Add(builder.UblOrderLine);
+      }
+      return this;
+    }
+
+    public IOrderBuilder BuildLines(object unknown)
+    {
+      return this;
+    }
+
+    public OrderBuilder BuyerCustomerParty(UblCustomerParty ublCustomerParty)
+    {
+      this._ublOrder.BuyerCustomerParty = ublCustomerParty;
+      return this;
+    }
+
+    public UblOrder GetUblOrder()
+    {
+      return _ublOrder;
+    }
+  }
+
+  public class BuyerPartyBuilder : IBuyerPartyBuilder
+  {
+    private UblCustomerParty _ublCustomerParty;
+
+    public BuyerPartyBuilder(UblCustomerParty ublCustomerParty)
+    {
+      _ublCustomerParty = ublCustomerParty;
+      _ublCustomerParty.Party = new UblParty();
+    }
+    public IBuyerPartyBuilder SetName(string name)
+    {
+      _ublCustomerParty.Party.PartyName = new UblPartyName(name);
+      return this;
+    }
+
+    public IBuyerPartyBuilder SetGln(string gln)
+    {
+      _ublCustomerParty.Party.PartyIdentifications = new List<UblPartyIdentification>()
+      {
+        new UblPartyIdentification(new UblIdentifier
+        {
+          Value = gln,
+          SchemeAgencyId = "GLN",
+          SchemeAgencyName = "INX"
+        })
+      };
+      return this;
+    }
+  }
+
+  public class LinesBuilder : ILinesBuilder
+  {
+    private List<UblOrderLine> _ublOrderLines;
+
+    public LinesBuilder(List<UblOrderLine> ublOrderLines)
+    {
+      _ublOrderLines = ublOrderLines;
+    }
+
+    public ILineBuilder BuildLine()
+    {
+      var line = new UblOrderLine();
+      _ublOrderLines.Add(line);
+      return new LineBuilder(line.LineItem);
+    }
+  }
+
+  
+  public class LineBuilder : ILineBuilder
+  {
+    private UblOrderLine _ublOrderLine = new UblOrderLine();
+
+    public LineBuilder()
+    {
+      _ublOrderLine.LineItem = new UblLineItem();
+    }
+    public LineBuilder(UblLineItem line)
+    {
+      _ublOrderLine.LineItem = line;
+    }
+
+    public ILineBuilder SetId(string id)
+    {
+      _ublOrderLine.LineItem.Id = new UblIdentifier(id);
+      return this;
+    }
+    
+    public ILineBuilder SetDescription(string description)
+    {
+      _ublOrderLine.LineItem.Item = new UblItem() { Description = new UblText(description) };
+      return this;
+    }
+
+    public UblOrderLine UblOrderLine
+    {
+      get { return _ublOrderLine; }
+    }
+  }
+
+  public class MyLine
+  {
+    public string Id { set; get; }
+    public string Name { set; get; }
+  }
+
+
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      List<MyLine> myLines = new List<MyLine>()
+      {
+        new MyLine() { Id = "Id1", Name = "Name1" },
+        new MyLine() { Id = "Id2", Name = "Name2" }
+      };
+
+      IOrderBuilder order = new OrderBuilder();
+
+      order.BuildId("1")
+        .BuildIssueDate(new DateTime())
+        .BuildIssueTime(new DateTime()).BuildBuyerParty(buyer => buyer.SetGln("60").SetName("CompanyName"))
+        .BuildLines(() => myLines.Select(a => new LineBuilder().SetDescription("d").SetId("id")));
+
+      var ublOrder = order.GetUblOrder();
+
+
+    }
+  }
+}
---
+++ Test.cs
@@ -0,0 +1,196 @@
+using System;
+using System.Collections.Generic;
+
+namespace FluentUbl.Example
+{
+  public class Test
+  {
+    public void CreateOrder(IOrderBuilder orderBuilder)
+    {
+
+      var lines = new List<string>();
+
+      orderBuilder.SetId("Id1")
+        .SetName("OrderName")
+        .SetCapacity(58)
+        .SetLines(linesBuilder =>
+        {
+          linesBuilder.SetName("SomeCoolName");
+          lines.ForEach((line => linesBuilder.AddLine(lineBuider => lineBuider.SetLine(line))));
+        })
+        .AddDelivery(deliveryBuilder => deliveryBuilder.SetStartDate(DateTime.Now)
+          .SetEndDate(DateTime.Now.AddDays(5)))
+        .AddDelivery(deliveryBuilder => deliveryBuilder.SetStartDate(DateTime.Now)
+          .SetEndDate(DateTime.Now.AddDays(5)))
+        .AddDelivery(deliveryBuilder => deliveryBuilder.SetStartDate(DateTime.Now)
+          .SetEndDate(DateTime.Now.AddDays(5)));
+    }
+
+    public void CreateDelivery(IDeliveryBuilder deliveryBuilder)
+    {
+      deliveryBuilder.SetStartDate(DateTime.Now)
+        .SetEndDate(DateTime.Now.AddDays(5));
+    }
+
+    public void CreateLines(ILinesBuilder linesBuilder)
+    {
+      linesBuilder.SetName("SomeCoolName")
+        .AddLine(CreateLine)
+        .AddLine(CreateLine)
+        .AddLine(CreateLine)
+        .AddLine(CreateLine);
+    }
+
+    public void CreateLine(ILineBuilder lineBuilder)
+    {
+      lineBuilder.SetLine("Line" + Guid.NewGuid());
+    }
+  }
+
+  public class OrderBuilder : IOrderBuilder
+  {
+    private string _id;
+    private string _name;
+    private int _capacity;
+    private object _lines;
+    private List<object> _deliveries;
+
+    public IOrderBuilder SetId(string id)
+    {
+      _id = id;
+      return this;
+    }
+
+    public IOrderBuilder SetName(string name)
+    {
+      _name = name;
+      return this;
+    }
+
+    public IOrderBuilder SetCapacity(int capacity)
+    {
+      _capacity = capacity;
+      return this;
+    }
+
+    public IOrderBuilder SetLines(Action<ILinesBuilder> action)
+    {
+      var linesBuilder = new LinesBuilder();
+      action(linesBuilder);
+      _lines = linesBuilder.Assemble();
+      return this;
+    }
+
+    public IOrderBuilder AddDelivery(Action<IDeliveryBuilder> action)
+    {
+      var deliveryBuilder = new DeliveryBuilder();
+      action(deliveryBuilder);
+      _deliveries.Add(deliveryBuilder.Assemble());
+      return this;
+    }
+
+    public object Assemble()
+    {
+      return new { Id = _id, Name = _name, Capacity = _capacity, Lines = _lines, Deliveries = _deliveries };
+    }
+  }
+
+  public class DeliveryBuilder : IDeliveryBuilder
+  {
+    private DateTime _startDate;
+    private DateTime _endDate;
+    
+    public IDeliveryBuilder SetStartDate(DateTime startDate)
+    {
+      _startDate = startDate;
+      return this;
+    }
+
+    public IDeliveryBuilder SetEndDate(DateTime endDate)
+    {
+      _endDate = endDate;
+      return this;
+    }
+
+    public object Assemble()
+    {
+      return new { StartDate = _startDate, EndDate = _endDate };
+    }
+  }
+
+  public class LinesBuilder : ILinesBuilder
+  {
+    private string _name;
+    private List<string> _lines;
+
+    public LinesBuilder()
+    {
+      _lines = new List<string>();
+    }
+
+    public ILinesBuilder SetName(string name)
+    {
+      _name = name;
+      return this;
+    }
+
+    public ILinesBuilder AddLine(Action<ILineBuilder> action)
+    {
+      var lineBuilder = new LineBuilder();
+      action(lineBuilder);
+      _lines.Add(lineBuilder.Assemble());
+      return this;
+    }
+
+    public object Assemble()
+    {
+      return new { Lines = _lines, Name = _name };
+    }
+  }
+
+  public class LineBuilder : ILineBuilder
+  {
+    private string _line;
+
+    public ILineBuilder SetLine(string line)
+    {
+      _line = line;
+      return this;
+    }
+
+    public string Assemble()
+    {
+      return _line;
+    }
+  }
+
+  public interface ILinesBuilder
+  {
+    ILinesBuilder SetName(string name);
+    ILinesBuilder AddLine(Action<ILineBuilder> action);
+    object Assemble();
+  }
+
+  public interface ILineBuilder
+  {
+    ILineBuilder SetLine(string line);
+    string Assemble();
+  }
+
+  public interface IOrderBuilder
+  {
+    IOrderBuilder SetId(string id);
+    IOrderBuilder SetName(string name);
+    IOrderBuilder SetCapacity(int capacity);
+    IOrderBuilder SetLines(Action<ILinesBuilder> action);
+    IOrderBuilder AddDelivery(Action<IDeliveryBuilder> action);
+    object Assemble();
+  }
+
+  public interface IDeliveryBuilder
+  {
+    IDeliveryBuilder SetStartDate(DateTime startDate);
+    IDeliveryBuilder SetEndDate(DateTime endDate);
+    object Assemble();
+  }
+}
